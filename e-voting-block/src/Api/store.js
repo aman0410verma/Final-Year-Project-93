02155dfcc16d1f53386a82b9358899458d771aa9
Aman Verma <112889066+aman0410verma@users.noreply.github.com>
2023-05-06 16:34:24 +0530

    Add files via upload

---
+++ e-voting-block/src/Api/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers } from "redux";
+import { accountReducer } from "../Reducers/AddAccountReducer";
+import { AddCandidateReducer } from "../Reducers/AddCandidatesReducer";
+import { ChangeStateReducer } from "../Reducers/ChangeStateReducers";
+import { evoteReducer } from "../Reducers/EvoteReducer";
+import { winnerReducer } from "../Reducers/WinnerReducer";
+const combined = combineReducers({
+  candidates: AddCandidateReducer,
+  changeState: ChangeStateReducer,
+  eVote: evoteReducer,
+  account:accountReducer,
+  winnerData:winnerReducer
+});
+export const store = createStore(combined);
+
+// react , react-redux , web3 js
+
+// solidiity , turffle , ganache
